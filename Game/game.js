19aab4e54c653c48cd444af4755de1a9793553eb
기나윤 <nayun@ginayun-ui-MacBookAir.local>
2024-02-03 21:03:46 +0900

    new page: game

--- Game/game.js
+++ Game/game.js
@@ -9,26 +9,10 @@ class Game {
 			<button class="main_header_bar__tab gameBtn __tab_active">Game</button>
 			<button class="main_header_bar__tab rankBtn">Rank</button>
 		</div>
-		<div class="main_welcome">Game page!</div>
-		<div class="main_body">
-			<ul class="main_list">
-				<li class="main_list_item">Rules of Pong</li>
-				<li class="main_list_item">How to play</li>
-				<li class="main_list_item">How to chat</li>
-				<li class="main_list_item">Tournament</li>
-			</ul>
+		<div class="main_game">
+			<button class="random_match">Random Match</button>
+			<button class="tourna_match">Tournament</button>
 		</div>
-		<footer>
-			<div class="footer_name">Team 42tsc</div>
-			<ul class="footer_list">
-				<li class="footer_list_item">hyeslim</li>
-				<li class="footer_list_item">youngmch</li>
-				<li class="footer_list_item">isunwoo</li>
-				<li class="footer_list_item">naki</li>
-				<li class="footer_list_item">jeelee</li>
-			</ul>
-			<div class="contacts">contacts: </div>
-		</footer>
 		`;
 	}
 }
