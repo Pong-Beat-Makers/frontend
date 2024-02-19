905aecd19105be07aeb8178d90329fd9b59f4cc0
기나윤 <nayun@ginayun-ui-MacBookAir.local>
2024-02-19 20:19:56 +0900

    fix: chat hangeul input double enter error

--- Chat/chatUtils.js
+++ Chat/chatUtils.js
@@ -130,7 +130,7 @@ export function initChatSocket() {
     };
 
     document.querySelector('#chat-message-input').focus();
-    document.querySelector('#chat-message-input').onkeyup = function(e) {
+    document.querySelector('#chat-message-input').onkeydown = function(e) {
         if (e.keyCode === 13) {  // enter, return
             document.querySelector('#chat-message-submit').click();
         }
