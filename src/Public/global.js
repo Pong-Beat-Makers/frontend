a50350bfc960372647a5ec46d619a93f00635598
기나윤 <nayun@ginayun-ui-MacBookAir.local>
2024-03-05 16:13:40 +0900

    chore: update FRONTEND port

--- src/Public/global.js
+++ src/Public/global.js
@@ -1,4 +1,4 @@
-export const FRONTEND = "http://127.0.0.1:5500";
+export const FRONTEND = "http://127.0.0.1:5501";
 export const BACKEND = "http://127.0.0.1:8000";
 export const LOCALHOST = "127.0.0.1";
 
